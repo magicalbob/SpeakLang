aa70b22d73e0e8d9e9cbcb442c4f6c7750b61512
Ian Ellis <ellisiana@gmail.com>
2024-06-13 22:03:12 +0100

    git rid of testscipt.sh and put steps directly in the pipeline

--- testscript.sh
+++
@@ -1,6 +0,0 @@
-#export PYTHONPATH=.
-pip install -r requirements.txt
-python3 -m unittest tests.test_speaklang
-~/.local/bin/coverage run -m unittest tests/test_speaklang
-~/.local/bin/coverage xml
-rm -rf __pycache__
