aeae1b395e492a74a5cedba475cc9bc644b41c70
Ian Ellis <ellisiana@gmail.com>
2024-06-13 15:16:18 +0100

    correct test file name

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,5 @@
 pip install -r requests.txt
 ./tests/speaklang_unittest.py
-~/.local/bin/coverage run -m unittest tests/speaklang_unittest.py
+~/.local/bin/coverage run -m unittest tests/test_speaklang.py
 ~/.local/bin/coverage xml
 rm -rf __pycache__
