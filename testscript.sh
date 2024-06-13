c5458f592e79403e73febbcd47967ab8920d4f2e
Ian Ellis <ellisiana@gmail.com>
2024-06-13 15:12:21 +0100

    requests no requirements

--- testscript.sh
+++ testscript.sh
@@ -1,4 +1,4 @@
-pip install -r requirements.txt
+pip install -r requests.txt
 ./tests/speaklang_unittest.py
 ~/.local/bin/coverage run -m unittest tests/speaklang_unittest.py
 ~/.local/bin/coverage xml
